67eefaa13b82b6cbf5d74eb1cbcc5e29ba431c56
junhkang <junhkang91@gmail.com>
2024-08-06 12:34:57 +0900

    feat : drop index on uninstall script

--- sql/uninstall_kor_search.sql
+++ sql/uninstall_kor_search.sql
@@ -1,4 +1,6 @@
 -- uninstall_kor_search.sql
 DROP FUNCTION IF EXISTS kor_like(text, text);
 DROP FUNCTION IF EXISTS kor_search_tsvector(text, text);
+DROP INDEX IF EXISTS idx_kor_search_keyword;
+DROP INDEX IF EXISTS idx_kor_search_synonyms;
 DROP TABLE IF EXISTS kor_search_word_transform;
